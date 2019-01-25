004789242f4104334a1f8aeef921f95490150a70
Jarred Capellman <jcapellman@hotmail.com>
2019-01-24 20:53:50 -0600

    Added: Initial checkin of the MainGame and Program classes

---
+++ src/DasSidescroller.UWP/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DasSidescroller.UWP
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<MainGame>();
+
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
