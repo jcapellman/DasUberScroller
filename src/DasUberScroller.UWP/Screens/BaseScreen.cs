f711e50cb7a44be1b6b141d530eb43050a52c8d3
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 13:33:04 -0600

    Added: Initial checkin of the BaseScreen class

---
+++ src/DasUberScroller.UWP/Screens/BaseScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+using DasUberScroller.UWP.Objects;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DasUberScroller.UWP.Screens
+{
+    public abstract class BaseScreen
+    {
+        private List<BaseObject> _gameObjects = new List<BaseObject>();
+
+        protected void AddObject(BaseObject obj)
+        {
+            _gameObjects.Add(obj);
+        }
+
+        public abstract bool LoadContent(GameContentManager gameContentManager, WindowContainer windowContainer);
+
+        public void RenderScreen(SpriteBatch spireBatch, GameContentManager gameContentManager)
+        {
+            foreach (var gameObject in _gameObjects)
+            {
+                gameObject.Render(spireBatch, gameContentManager);
+            }
+        }
+
+        public void UpdateWindow(WindowContainer windowContainer)
+        {
+            foreach (var gameObject in _gameObjects)
+            {
+                gameObject.UpdateWindow(windowContainer);
+            }
+        }
+
+        public void UpdateScreen(KeyboardState keyboardState, GameTime gameTime)
+        {
+            foreach (var gameObject in _gameObjects)
+            {
+                gameObject.Update(keyboardState, gameTime);
+            }
+        }
+    }
+}
