e1f406f3a3681e91dcac638b84c89fe9f7a7be5b
Jarred Capellman <jcapellman@hotmail.com>
2019-01-25 23:52:13 -0600

    Added: Animated Clouds support

--- src/DasUberScroller.UWP/Objects/Level.cs
+++ src/DasUberScroller.UWP/Objects/Level.cs
@@ -6,15 +6,35 @@ namespace DasUberScroller.UWP.Objects
     public class Level
     {
         private Texture2D _road;
+        private int _yResolution;
+        private Texture2D _clouds;
+        private Texture2D _animatedClouds;
+        private int _animationFrameX = 0;
 
-        public Level(Texture2D road)
+        public Level(Texture2D road, Texture2D clouds, Texture2D animatedClouds, int yResolution)
         {
             _road = road;
+            _clouds = clouds;
+            _animatedClouds = animatedClouds;
+            _yResolution = yResolution;
         }
 
+        [System.Obsolete]
         public void Render(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_road, new Vector2(0, 10), new Rectangle(0, 0, 1920, 100), Color.White);
+            if (_animationFrameX < -1920)
+            {
+                _animationFrameX = 0;
+            }
+            else
+            {
+                _animationFrameX -= 1;
+            }
+
+            spriteBatch.Draw(_clouds, new Rectangle(0, 0, 1920, 1080), Color.White);
+            spriteBatch.Draw(_animatedClouds, new Vector2(0 + _animationFrameX, 0));
+
+            spriteBatch.Draw(_road, new Rectangle(0, _yResolution - 184, 1920, 184), Color.White);
         }
     }
 }
