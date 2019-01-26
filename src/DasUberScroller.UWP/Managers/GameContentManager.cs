43e91254844301e3768d99be071af265100de0a7
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 13:11:40 -0600

    Added: Initial work on the GameContentManager class

---
+++ src/DasUberScroller.UWP/Managers/GameContentManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DasUberScroller.UWP.Managers
+{
+    public class GameContentManager
+    {
+        private Dictionary<string, Texture2D> _textures;
+
+        private readonly ContentManager _contentManager;
+
+        public GameContentManager(ContentManager contentManager)
+        {
+            _textures = new Dictionary<string, Texture2D>();
+
+            _contentManager = contentManager;
+        }
+
+        public bool LoadTexture(string name)
+        {
+            if (_textures.ContainsKey(name))
+            {
+                return true;
+            }
+
+            _textures.Add(name, _contentManager.Load<Texture2D>(name));
+
+            return true;
+        }
+
+        public Texture2D GetTexture(string name) => !_textures.ContainsKey(name) ? null : _textures[name];
+    }
+}
