241fcb34f5e2eb1dcb39f748f06dd575050138f7
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 13:32:45 -0600

    Added: Initial work on the GameScreenManager class

---
+++ src/DasUberScroller.UWP/Managers/GameScreenManager.cs
@@ -0,0 +1,44 @@
+﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Screens;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DasUberScroller.UWP.Managers
+{
+    public class GameScreenManager
+    {
+        private GameContentManager _gameContentManager;
+
+        private BaseScreen _currentScreen;
+
+        public GameScreenManager(ContentManager contentManager)
+        {
+            _gameContentManager = new GameContentManager(contentManager);
+        }
+
+        public void LoadScreen(BaseScreen screen, WindowContainer windowContainer)
+        {
+            _currentScreen = screen;
+
+            _currentScreen.LoadContent(_gameContentManager, windowContainer);
+        }
+
+        public void RenderCurrentScreen(SpriteBatch spriteBatch)
+        {
+            _currentScreen.RenderScreen(spriteBatch, _gameContentManager);
+        }
+
+        public void UpdateCurrentScreen(KeyboardState state, GameTime gameTime)
+        {
+            _currentScreen.UpdateScreen(state, gameTime);
+        }
+
+        public void UpdateCurrentScreenWindow(WindowContainer windowContainer)
+        {
+            _currentScreen.UpdateWindow(windowContainer);
+        }
+    }
+}
