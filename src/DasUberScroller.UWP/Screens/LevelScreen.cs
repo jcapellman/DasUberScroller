9d0007d7d6c0e10f04ca416137e70dff16c5fec2
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 13:33:26 -0600

    Added: Initial work on the LevelScreen class

---
+++ src/DasUberScroller.UWP/Screens/LevelScreen.cs
@@ -0,0 +1,20 @@
+﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+using DasUberScroller.UWP.Objects;
+
+namespace DasUberScroller.UWP.Screens
+{
+    public class LevelScreen : BaseScreen
+    {
+        public override bool LoadContent(GameContentManager gameContentManager, WindowContainer windowContainer)
+        {
+            var player = new Player(gameContentManager, windowContainer);
+            var level = new Level(gameContentManager, windowContainer);
+
+            AddObject(level);
+            AddObject(player);
+
+            return true;
+        }
+    }
+}
