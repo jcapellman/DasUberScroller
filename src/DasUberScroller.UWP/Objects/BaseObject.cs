e185bbab8f46319283f2c3a5b5d8e55264da1f1e
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 10:13:52 -0600

    Added: Abstract methods for the BaseObjects

--- src/DasUberScroller.UWP/Objects/BaseObject.cs
+++ src/DasUberScroller.UWP/Objects/BaseObject.cs
@@ -1,14 +1,27 @@
 ﻿using DasUberScroller.UWP.Containers;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace DasUberScroller.UWP.Objects
 {
-    public class BaseObject
+    public abstract class BaseObject
     {
-        protected readonly WindowContainer WindowContainer;
+        protected WindowContainer WindowContainer;
 
         protected BaseObject(WindowContainer windowContainer)
         {
             WindowContainer = windowContainer;
         }
+
+        public void UpdateWindow(WindowContainer windowContainer)
+        {
+            WindowContainer = windowContainer;
+        }
+
+        public abstract void Render(SpriteBatch spriteBatch);
+        
+        public abstract void Update(KeyboardState keyboardState, GameTime gameTime);
     }
 }
