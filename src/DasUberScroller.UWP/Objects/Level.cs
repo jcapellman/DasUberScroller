20916efad4e82a334be5f4a4b427b9735d37bc0f
Jarred Capellman <jcapellman@hotmail.com>
2019-01-25 21:49:04 -0600

    Added: Initial work on the Level class

---
+++ src/DasUberScroller.UWP/Objects/Level.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DasUberScroller.UWP.Objects
+{
+    public class Level
+    {
+        private Texture2D _road;
+
+        public Level(Texture2D road)
+        {
+            _road = road;
+        }
+
+        public void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_road, new Vector2(0, 10), new Rectangle(0, 0, 1920, 100), Color.White);
+        }
+    }
+}
