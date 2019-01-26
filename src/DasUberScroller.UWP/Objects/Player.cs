2d90b4c5335f04fe5190b48af46f087e83d7c593
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 09:39:39 -0600

    Change: Refactored the Player class to utilize the WindowContainer class

--- src/DasUberScroller.UWP/Objects/Player.cs
+++ src/DasUberScroller.UWP/Objects/Player.cs
@@ -1,22 +1,23 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DasUberScroller.UWP.Containers;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace DasUberScroller.UWP.Objects
 {
-    public class Player
+    public class Player : BaseObject
     {
         private Texture2D _playerSheet;
         private int column;
         private int row;
-        private int _yResolution;
         private int _xPosition;
 
         private double _lastMs = 0;
 
         private const double Delay = 34;
 
-        public Player(Texture2D texture, int yResolution)
+        public Player(Texture2D texture, WindowContainer windowContainer) : base(windowContainer)
         {
             _playerSheet = texture;
 
@@ -24,14 +25,12 @@ namespace DasUberScroller.UWP.Objects
             row = 1;
 
             _xPosition = 0;
-
-            _yResolution = yResolution;
         }
 
         [System.Obsolete]
         public void Render(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_playerSheet, position: new Vector2(_xPosition, _yResolution - 300),
+            spriteBatch.Draw(_playerSheet, position: new Vector2(_xPosition, WindowContainer.ResolutionY - 300),
                 sourceRectangle: new Rectangle(column * 80, row * 100, 80, 100), color: Color.White, scale: new Vector2(3.0f, 3.0f));
         }
 
