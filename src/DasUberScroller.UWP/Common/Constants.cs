a785580629e6ae86d0b3de7811fd7bb4ebd4d4da
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 12:50:42 -0600

    Added: Target Scaling to 1920x1080

---
+++ src/DasUberScroller.UWP/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace DasUberScroller.UWP.Common
+{
+    public static class Constants
+    {
+        public const int BASE_RESOLUTION_X = 1920;
+
+        public const int BASE_RESOLUTION_Y = 1080;
+    }
+}
