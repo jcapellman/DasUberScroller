4fafb397fdd0eabd1c5872c8bb5b1ca681967257
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 12:51:16 -0600

    Added: ScaleResolutionX and ScaleResolutionY auto-properties to the WindowContainer class

--- src/DasUberScroller.UWP/Containers/WindowContainer.cs
+++ src/DasUberScroller.UWP/Containers/WindowContainer.cs
@@ -1,9 +1,15 @@
-﻿namespace DasUberScroller.UWP.Containers
+﻿using DasUberScroller.UWP.Common;
+
+namespace DasUberScroller.UWP.Containers
 {
     public class WindowContainer
     {
         public int ResolutionX { get; set; }
 
         public int ResolutionY { get; set; }
+
+        public float ScaleResolutionX => ResolutionX / (float)Constants.BASE_RESOLUTION_X;
+
+        public float ScaleResolutionY => ResolutionY / (float)Constants.BASE_RESOLUTION_Y;
     }
 }
