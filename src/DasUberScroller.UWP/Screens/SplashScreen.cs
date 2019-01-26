c97813f3884a13788e22a490ca9572a24290466f
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 15:03:14 -0600

    Added: Initial work on the SplashScreen class

---
+++ src/DasUberScroller.UWP/Screens/SplashScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+using DasUberScroller.UWP.Objects;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace DasUberScroller.UWP.Screens
+{
+    public class SplashScreen : BaseScreen
+    {
+        private const string TextureName = "Splash";
+
+        public override bool LoadContent(GameContentManager gameContentManager, WindowContainer windowContainer)
+        {
+            gameContentManager.LoadTexture(TextureName);
+
+            AddObject(new FSBillboard(TextureName, windowContainer));
+
+            return true;
+        }
+
+        public override void UpdateScreen(KeyboardState keyboardState, GameTime gameTime)
+        {
+            if (!keyboardState.GetPressedKeys().Any())
+            {
+                return;
+            }
+
+            OnChangeScreen(null, new LevelScreen());
+        }
+    }
+}
