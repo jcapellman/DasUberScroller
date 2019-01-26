1f712322a5df9cf38689d94df28f023cf4d3eabf
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 09:40:13 -0600

    Added: WindowContainer class

---
+++ src/DasUberScroller.UWP/Containers/WindowContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace DasUberScroller.UWP.Containers
+{
+    public class WindowContainer
+    {
+        public int ResolutionX { get; set; }
+
+        public int ResolutionY { get; set; }
+    }
+}
