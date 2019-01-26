76e0e47ad06eb3ab93db623c208be27766f87157
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 09:39:11 -0600

    Change: Refactored the Level class to utilize the WindowContainer class

--- src/DasUberScroller.UWP/Objects/Level.cs
+++ src/DasUberScroller.UWP/Objects/Level.cs
@@ -1,40 +1,40 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DasUberScroller.UWP.Containers;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace DasUberScroller.UWP.Objects
 {
-    public class Level
+    public class Level : BaseObject
     {
         private Texture2D _road;
-        private int _yResolution;
         private Texture2D _clouds;
         private Texture2D _animatedClouds;
         private int _animationFrameX = 0;
 
-        public Level(Texture2D road, Texture2D clouds, Texture2D animatedClouds, int yResolution)
+        public Level(Texture2D road, Texture2D clouds, Texture2D animatedClouds, WindowContainer windowContainer) : base(windowContainer)
         {
             _road = road;
             _clouds = clouds;
             _animatedClouds = animatedClouds;
-            _yResolution = yResolution;
         }
 
         [System.Obsolete]
         public void Render(SpriteBatch spriteBatch)
         {
-            if (_animationFrameX < -1920)
+            if (_animationFrameX < (-1 * WindowContainer.ResolutionX))
             {
-                _animationFrameX = 0;
+                _animationFrameX = WindowContainer.ResolutionX;
             }
             else
             {
                 _animationFrameX -= 1;
             }
 
-            spriteBatch.Draw(_clouds, new Rectangle(0, 0, 1920, 1080), Color.White);
+            spriteBatch.Draw(_clouds, new Rectangle(0, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), Color.White);
             spriteBatch.Draw(_animatedClouds, new Vector2(0 + _animationFrameX, 0));
 
-            spriteBatch.Draw(_road, new Rectangle(0, _yResolution - 184, 1920, 184), Color.White);
+            spriteBatch.Draw(_road, new Rectangle(0, WindowContainer.ResolutionY - 184, WindowContainer.ResolutionX, 184), Color.White);
         }
     }
 }
