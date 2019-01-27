cf5a269bc70a3cd6aeebc121409d05c1ff9c6c7c
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 21:36:39 -0600

    Change: Optimized the LoadTexture method

--- src/DasUberScroller.UWP/Managers/GameContentManager.cs
+++ src/DasUberScroller.UWP/Managers/GameContentManager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 
+using DasUberScroller.UWP.Containers;
+
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -18,16 +20,18 @@ namespace DasUberScroller.UWP.Managers
             _contentManager = contentManager;
         }
 
-        public bool LoadTexture(string name)
+        public (bool Loaded, TextureContainer Texture) LoadTexture(string name)
         {
-            if (_textures.ContainsKey(name))
+            if (!_textures.ContainsKey(name))
             {
-                return true;
+                _textures.Add(name, _contentManager.Load<Texture2D>(name));
             }
-
-            _textures.Add(name, _contentManager.Load<Texture2D>(name));
-
-            return true;
+            
+            return (true, new TextureContainer {
+                Width = _textures[name].Width,
+                Height = _textures[name].Height,
+                Name = name
+            });
         }
 
         public Texture2D GetTexture(string name) => !_textures.ContainsKey(name) ? null : _textures[name];
