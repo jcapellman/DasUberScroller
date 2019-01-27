511490d7588b50798ef0ec2e82722505898dc3d4
Jarred Capellman <jcapellman@hotmail.com>
2019-01-27 10:08:49 -0600

    Added: Initial work on the LevelJSON class

---
+++ src/DasUberScroller.UWP/JSONObjects/LevelJSON.cs
@@ -0,0 +1,13 @@
+﻿namespace DasUberScroller.UWP.JSONObjects
+{
+    public class LevelJSON
+    {
+        public string Title { get; set; }
+
+        public string TextureAtmosphere { get; set; }
+
+        public string TextureAtmosphereOverlay { get; set; }
+
+        public string TextureFloor { get; set; }
+    }
+}
