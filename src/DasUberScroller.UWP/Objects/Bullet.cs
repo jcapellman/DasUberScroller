1d08920d524fb16c47c27b33129a866a50d37440
Jarred Capellman <jcapellman@hotmail.com>
2019-01-26 21:42:51 -0600

    Added: Initial work on the Bullet class

---
+++ src/DasUberScroller.UWP/Objects/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DasUberScroller.UWP.Objects
+{
+    public class Bullet : BaseObject
+    {
+        private const string TextureName = "bullet";
+
+        public Bullet(GameContentManager gameContentManager, WindowContainer windowContainer) : base(windowContainer)
+        {
+            gameContentManager.LoadTexture(TextureName);
+        }
+        
+        public override void Render(SpriteBatch spriteBatch, GameContentManager gameContentManager)
+        {
+        }
+
+        public override void Update(KeyboardState keyboardState, GameTime gameTime)
+        {
+
+        }
+    }
+}
