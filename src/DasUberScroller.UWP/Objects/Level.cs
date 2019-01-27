b4db3ec6d58c3c1e197312423600742c8ef907a2
Jarred Capellman <jcapellman@hotmail.com>
2019-01-27 10:07:51 -0600

    Added: Initial work on Level loading from a JSON file

--- src/DasUberScroller.UWP/Objects/Level.cs
+++ src/DasUberScroller.UWP/Objects/Level.cs
@@ -1,40 +1,67 @@
-﻿using DasUberScroller.UWP.Containers;
+﻿using System;
+using System.IO;
+
+using Windows.Storage;
+
+using DasUberScroller.UWP.Common;
+using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.JSONObjects;
 using DasUberScroller.UWP.Managers;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using Newtonsoft.Json;
+
 namespace DasUberScroller.UWP.Objects
 {
     public class Level : BaseObject
     {
         private int _animationFrameX = 0;
-
-        private const string TextureFloor = "road";
-        private const string TextureClouds = "clouds1"; 
-        private const string TextureAnimatedClouds = "clouds2";
-
+        
         private readonly TextureContainer _floorTextureContainer;
 
-        public Level(GameContentManager contentManager, WindowContainer windowContainer) : base(windowContainer)
+        private LevelJSON _levelJson;
+
+        private LevelJSON LoadLevel(string levelName)
         {
-            var (loaded, texture) = contentManager.LoadTexture(TextureFloor);
+            var filePath = Path.Combine(Constants.PATH_LEVELS, $"{levelName}{Constants.FILE_EXTENSION_LEVEL}");
+
+            var appUri = new Uri(filePath);
+            var anjFile = StorageFile.GetFileFromApplicationUriAsync(appUri).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+            var jsonText = FileIO.ReadTextAsync(anjFile).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            return JsonConvert.DeserializeObject<JSONObjects.LevelJSON>(jsonText);            
+        }
+
+        public Level(string levelName, GameContentManager contentManager, WindowContainer windowContainer) : base(windowContainer)
+        {
+            _levelJson = LoadLevel(levelName);
+
+            var (loaded, texture) = contentManager.LoadTexture(_levelJson.TextureFloor);
 
             if (loaded)
             {
                 _floorTextureContainer = texture;
             }
 
-            contentManager.LoadTexture(TextureClouds);
-            contentManager.LoadTexture(TextureAnimatedClouds);
+            if (!string.IsNullOrEmpty(_levelJson.TextureAtmosphere))
+            {
+                contentManager.LoadTexture(_levelJson.TextureAtmosphere);
+            }
+
+            if (!string.IsNullOrEmpty(_levelJson.TextureAtmosphereOverlay))
+            {
+                contentManager.LoadTexture(_levelJson.TextureAtmosphereOverlay);
+            }
         }
 
         public override void Render(SpriteBatch spriteBatch, GameContentManager gameContentManager)
         {
-            Draw(TextureClouds, new Rectangle(0, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
+            Draw(_levelJson.TextureAtmosphere, new Rectangle(0, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
 
-            Draw(TextureAnimatedClouds, new Rectangle(0 + _animationFrameX, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
+            Draw(_levelJson.TextureAtmosphereOverlay, new Rectangle(0 + _animationFrameX, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
             
             Draw(_floorTextureContainer.Name, 
                 new Rectangle(0, 0, WindowContainer.ResolutionX, _floorTextureContainer.Height), 
