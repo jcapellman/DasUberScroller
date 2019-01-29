0344e9dab4ffa963b120f5f7ed93aa6f2edd5222
Jarred Capellman <jcapellman@hotmail.com>
2019-01-28 21:21:58 -0600

    Change: Renamed AnimatedAtmosphere to LevelObjects namespace

--- src/DasUberScroller.UWP/Objects/LevelObjects/AnimatedAtmosphere.cs
+++ src/DasUberScroller.UWP/Objects/LevelObjects/AnimatedAtmosphere.cs
@@ -1,28 +1,24 @@
 ﻿using DasUberScroller.UWP.Containers;
 using DasUberScroller.UWP.Managers;
+using DasUberScroller.UWP.Objects.LevelObjects.Base;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace DasUberScroller.UWP.Objects
+namespace DasUberScroller.UWP.Objects.LevelObjects
 {
-    public class AnimatedAtmosphere : BaseObject
+    public class AnimatedAtmosphere : LevelObject
     {
         private int _animationFrameX = 0;
-
-        private string _textureName;
-
-        public AnimatedAtmosphere(string textureName, GameContentManager contentManager, WindowContainer windowContainer) : base(windowContainer)
+        
+        public AnimatedAtmosphere(string textureName, GameContentManager contentManager, WindowContainer windowContainer) : base(textureName, contentManager, windowContainer)
         {
-            _textureName = textureName;
-
-            contentManager.LoadTexture(textureName);
         }
 
         public override void Render(SpriteBatch spriteBatch, GameContentManager gameContentManager)
         {
-            Draw(_textureName, new Rectangle(0 + _animationFrameX, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
+            Draw(TextureContainer.Name, new Rectangle(0 + _animationFrameX, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
         }
 
         public override void Update(KeyboardState keyboardState, GameTime gameTime)
