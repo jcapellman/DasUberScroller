90a3b1fb16311a1cfffb5010888c05d0767ed9ab
Jarred Capellman <jcapellman@hotmail.com>
2019-01-28 20:30:30 -0600

    Added: Initial work on the Atmosphere class

---
+++ src/DasUberScroller.UWP/Objects/Atmosphere.cs
@@ -0,0 +1,30 @@
+﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DasUberScroller.UWP.Objects
+{
+    public class Atmosphere : BaseObject
+    {
+        private string _textureName;
+
+        public Atmosphere(string textureName, GameContentManager contentManager, WindowContainer windowContainer) : base(windowContainer)
+        {
+            _textureName = textureName;
+
+            contentManager.LoadTexture(textureName);
+        }
+
+        public override void Render(SpriteBatch spriteBatch, GameContentManager gameContentManager)
+        {
+            Draw(_textureName, new Rectangle(0, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY), spriteBatch, gameContentManager);
+        }
+
+        public override void Update(KeyboardState keyboardState, GameTime gameTime)
+        {
+        }
+    }
+}
