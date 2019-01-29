3d89a68af746e1a7bb554c5f1bd2b7a922861f78
Jarred Capellman <jcapellman@hotmail.com>
2019-01-28 21:43:21 -0600

    Added: Initial work on the LevelScreenContentJSON class

---
+++ src/DasUberScroller.UWP/JSONObjects/LevelScreenContentJSON.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DasUberScroller.UWP.JSONObjects
+{
+    public class LevelScreenContentJSON
+    {
+        public List<LevelItemJSON> Items { get; set; }
+    }
+}
