3326dfdfa71e25f99937648608031e1a5e29eab5
Jarred Capellman <jcapellman@hotmail.com>
2019-01-28 21:42:55 -0600

    Added: Initial work on the LevelItemJSON class

---
+++ src/DasUberScroller.UWP/JSONObjects/LevelItemJSON.cs
@@ -0,0 +1,11 @@
+﻿using DasUberScroller.UWP.Enums;
+
+namespace DasUberScroller.UWP.JSONObjects
+{
+    public class LevelItemJSON
+    {
+        public LevelContentTypes LevelContentType { get; set; }
+
+        public string TextureName { get; set; }
+    }
+}
