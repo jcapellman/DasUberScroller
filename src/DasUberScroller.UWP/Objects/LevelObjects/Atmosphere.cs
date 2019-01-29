0a7ae69f4a968c74446414acd7dd3ec9a1bdfed1
Jarred Capellman <jcapellman@hotmail.com>
2019-01-28 21:22:26 -0600

    Added: Atmosphere and Floor classes

---
+++ src/DasUberScroller.UWP/Objects/LevelObjects/Atmosphere.cs
@@ -0,0 +1,22 @@
+﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+using DasUberScroller.UWP.Objects.LevelObjects.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DasUberScroller.UWP.Objects.LevelObjects
+{
+    public class Atmosphere : LevelObject
+    {
+        public Atmosphere(string textureName, GameContentManager contentManager, WindowContainer windowContainer) : base(textureName, contentManager, windowContainer)
+        {
+        }
+
+        public override void Render(SpriteBatch spriteBatch, GameContentManager gameContentManager)
+        {
+            Draw(TextureContainer.Name, new Rectangle(0, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY),
+                spriteBatch, gameContentManager);
+        }
+    }
+}
---
+++ src/DasUberScroller.UWP/Objects/LevelObjects/Floor.cs
@@ -0,0 +1,26 @@
+﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Managers;
+using DasUberScroller.UWP.Objects.LevelObjects.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DasUberScroller.UWP.Objects.LevelObjects
+{
+    public class Floor : LevelObject
+    {
+        public Floor(string textureName, GameContentManager contentManager, WindowContainer windowContainer) : base(textureName, contentManager, windowContainer)
+        {
+        }
+
+        public override void Render(SpriteBatch spriteBatch, GameContentManager gameContentManager)
+        {
+            Draw(TextureContainer.Name,
+                new Rectangle(0, 0, WindowContainer.ResolutionX, TextureContainer.Height),
+                new Vector2(0, WindowContainer.ResolutionY - TextureContainer.Height),
+                1.0f,
+                spriteBatch,
+                gameContentManager);
+        }
+    }
+}
