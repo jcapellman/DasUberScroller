927e3bf7442e9133c716e76969fe900abb4ed2bc
Jarred Capellman <jcapellman@hotmail.com>
2019-01-29 21:07:01 -0600

    Added: ContentType implemented in the Atmosphere class

--- src/DasUberScroller.UWP/Objects/LevelObjects/Atmosphere.cs
+++ src/DasUberScroller.UWP/Objects/LevelObjects/Atmosphere.cs
@@ -1,4 +1,5 @@
 ﻿using DasUberScroller.UWP.Containers;
+using DasUberScroller.UWP.Enums;
 using DasUberScroller.UWP.Managers;
 using DasUberScroller.UWP.Objects.LevelObjects.Base;
 
@@ -9,6 +10,8 @@ namespace DasUberScroller.UWP.Objects.LevelObjects
 {
     public class Atmosphere : LevelObject
     {
+        public Atmosphere() : base(null, null, null) { }
+
         public Atmosphere(string textureName, GameContentManager contentManager, WindowContainer windowContainer) : base(textureName, contentManager, windowContainer)
         {
         }
@@ -18,5 +21,7 @@ namespace DasUberScroller.UWP.Objects.LevelObjects
             Draw(TextureContainer.Name, new Rectangle(0, 0, WindowContainer.ResolutionX, WindowContainer.ResolutionY),
                 spriteBatch, gameContentManager);
         }
+
+        public override LevelContentTypes ContentType => LevelContentTypes.Atmosphere;
     }
 }
