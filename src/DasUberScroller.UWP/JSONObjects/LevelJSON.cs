dbaf4a38355930c36e1ccb2017c387de85f167cb
Jarred Capellman <jcapellman@hotmail.com>
2019-01-30 20:22:03 -0600

    Change: LevelJSON class now has a static LoadLevel method

--- src/DasUberScroller.UWP/JSONObjects/LevelJSON.cs
+++ src/DasUberScroller.UWP/JSONObjects/LevelJSON.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Windows.Storage;
+
+using DasUberScroller.UWP.Common;
+
+using Newtonsoft.Json;
 
 namespace DasUberScroller.UWP.JSONObjects
 {
@@ -7,5 +15,16 @@ namespace DasUberScroller.UWP.JSONObjects
         public string Title { get; set; }
 
         public List<LevelScreenContentJSON> Screens { get; set; }
+
+        public static LevelJSON LoadLevel(string levelName)
+        {
+            var filePath = Path.Combine(Constants.PATH_LEVELS, $"{levelName}{Constants.FILE_EXTENSION_LEVEL}");
+
+            var appUri = new Uri(filePath);
+            var anjFile = StorageFile.GetFileFromApplicationUriAsync(appUri).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+            var jsonText = FileIO.ReadTextAsync(anjFile).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            return JsonConvert.DeserializeObject<JSONObjects.LevelJSON>(jsonText);
+        }
     }
 }
