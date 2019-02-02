5ca842e59db23971de306071df20d6d99f4c25fe
Jarred Capellman <jcapellman@hotmail.com>
2019-02-01 21:36:36 -0600

    Added: Initial work on the DIContainer class

---
+++ src/DasUberScroller.lib/DI/DIContainer.cs
@@ -0,0 +1,22 @@
+﻿using DasUberScroller.lib.PlatformAbstractions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DasUberScroller.lib.DI
+{
+    public class DIContainer
+    {
+        private static ServiceProvider _container;
+
+        public static T GetDIService<T>() => _container.GetService<T>();
+
+        public static void Initialize(IFileSystem fileSystem)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(fileSystem);
+
+            _container = serviceCollection.BuildServiceProvider();
+        }
+    }
+}
