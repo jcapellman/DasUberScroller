b30f6f6e1d765c6beeee5f106fac11e383a67b01
Jarred Capellman <jcapellman@hotmail.com>
2019-02-01 21:38:35 -0600

    Added: Initial commit of the UWPFileSystem class

---
+++ src/DasUberScroller.UWP/PlatformImplementations/UWPFileSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Windows.Storage;
+
+using DasUberScroller.lib.PlatformAbstractions;
+
+namespace DasUberScroller.UWP.PlatformImplementations
+{
+    public class UWPFileSystem : IFileSystem
+    {
+        public string ReadTextFromFile(string fileName)
+        {
+            var appUri = new Uri(fileName);
+
+            var anjFile = StorageFile.GetFileFromApplicationUriAsync(appUri).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            return FileIO.ReadTextAsync(anjFile).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
