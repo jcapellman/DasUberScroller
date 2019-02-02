7ebaa9b2d625f5194fbee9c8f08ff6a1c4fb645e
Jarred Capellman <jcapellman@hotmail.com>
2019-02-02 12:30:25 -0600

    Added: Initial work on the ReturnSet class

---
+++ src/DasUberScroller.library/Common/ReturnSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DasUberScroller.library.Common
+{
+    public class ReturnSet<T>
+    {
+        public T Value { get; }
+
+        public Exception Error { get; }
+
+        public bool HasErrorOrNull => Error != null || Value == null;
+
+        public ReturnSet(T objectValue)
+        {
+            Value = objectValue;
+        }
+
+        public ReturnSet(Exception exception)
+        {
+            Error = exception;
+        }
+    }
+}
