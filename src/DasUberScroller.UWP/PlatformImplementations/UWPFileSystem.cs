3a3cd8e40f9682d96b509d21b80041f2a3aba478
Jarred Capellman <jcapellman@hotmail.com>
2019-02-02 12:30:43 -0600

    Added: Error Handling in the UWPFileSystem class

--- src/DasUberScroller.UWP/PlatformImplementations/UWPFileSystem.cs
+++ src/DasUberScroller.UWP/PlatformImplementations/UWPFileSystem.cs
@@ -1,20 +1,44 @@
 ﻿using System;
+using System.IO;
 
 using Windows.Storage;
 
 using DasUberScroller.lib.PlatformAbstractions;
+using DasUberScroller.library.Common;
 
 namespace DasUberScroller.UWP.PlatformImplementations
 {
     public class UWPFileSystem : IFileSystem
     {
-        public string ReadTextFromFile(string fileName)
+        public ReturnSet<string> ReadTextFromFile(string fileName)
         {
-            var appUri = new Uri(fileName);
+            try
+            {
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    throw new ArgumentNullException(fileName);
+                }
 
-            var anjFile = StorageFile.GetFileFromApplicationUriAsync(appUri).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+                var appUri = new Uri(fileName);
 
-            return FileIO.ReadTextAsync(anjFile).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+                var anjFile = StorageFile.GetFileFromApplicationUriAsync(appUri).AsTask().ConfigureAwait(false)
+                    .GetAwaiter().GetResult();
+
+                if (anjFile == null)
+                {
+                    throw new FileNotFoundException(fileName);
+                }
+
+                return new ReturnSet<string>(FileIO.ReadTextAsync(anjFile).AsTask().ConfigureAwait(false).GetAwaiter().GetResult());
+            }
+            catch (ArgumentNullException argumentNullException)
+            {
+                return new ReturnSet<string>(argumentNullException);
+            }
+            catch (FileNotFoundException fileNotFoundException)
+            {
+                return new ReturnSet<string>(fileNotFoundException);
+            }
         }
     }
 }
