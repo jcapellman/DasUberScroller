e9259db0a3269d57ea1062fdf82e24f0c2ada948
Jarred Capellman <jcapellman@hotmail.com>
2019-02-02 19:21:44 -0600

    Change: Removed the PATH_LEVELS property in the Constants class

--- src/DasUberScroller.library/Common/Constants.cs
+++ src/DasUberScroller.library/Common/Constants.cs
@@ -5,9 +5,7 @@
         public const int BASE_RESOLUTION_X = 1920;
 
         public const int BASE_RESOLUTION_Y = 1080;
-
-        public const string PATH_LEVELS = "ms-appx:///Assets/Levels/";
-
+        
         public const string DEFAULT_LEVEL_NAME = "E1M1";
 
         public const string FILE_EXTENSION_LEVEL = ".json";
